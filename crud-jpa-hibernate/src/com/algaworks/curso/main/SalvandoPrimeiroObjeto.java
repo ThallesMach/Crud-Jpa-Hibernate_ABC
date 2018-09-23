e377a61d4c2b99fcd8edc6113b88a8295135e51d
Thalles Machado <35805076+ThallesMach@users.noreply.github.com>
2018-09-22 23:57:20 -0300

    _add proj inicial Crud
    CRUD inicial do curso crud-jpa-hibernate da Algaworks

---
+++ crud-jpa-hibernate/src/com/algaworks/curso/main/SalvandoPrimeiroObjeto.java
@@ -0,0 +1,15 @@
+package com.algaworks.curso.main;
+
+import javax.persistence.EntityManager;
+
+public class SalvandoPrimeiroObjeto {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Primeiro Teste_");
+		
+//		EntityManager em = null;
+		
+	}
+
+}
